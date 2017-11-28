a4284bea06e68d62a7cb9370b99e76f70a0cc273
RBailleux <robin.bailleux@gmail.com>
2017-11-28 18:34:31 +0100

    form style and validation

--- app.js
+++ app.js
@@ -5,27 +5,42 @@ const app = require("express")();
 const stripe = require("stripe")(keySecret);
 const PORT = process.env.PORT || 5000;
 
+var express = require("express");
+
 app.set("view engine", "pug");
 app.use(require("body-parser").urlencoded({extended: false}));
 
-app.get("/", (req, res) =>
-  res.render("index.pug", {keyPublishable}));
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    console.log(123);
+    res.render("index.pug", {keyPublishable});
+});
 
 app.post("/charge", (req, res) => {
+  console.log(req.body)
   let amount = 500;
+  
+  var pugVariables = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    address: req.body.address+' à '+req.body.city,
+    amount: (amount/100)+'€'
+  };
 
   stripe.customers.create({
-     email: req.body.stripeEmail,
+     email: req.body.email,
     source: req.body.stripeToken
   })
   .then(customer =>
     stripe.charges.create({
       amount,
-      description: "Sample Charge",
-         currency: "eur",
-         customer: customer.id
+      description: "Exercice",
+      currency: "eur",
+      customer: customer.id
     }))
-  .then(charge => res.render("charge.pug"));
+  .then(charge => res.render("charge.pug", pugVariables));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
